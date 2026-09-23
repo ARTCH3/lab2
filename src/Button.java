b5260b98ff9e54ac63f31d646076a84b29d5fab4
ARTCH3 <artch3@icloud.com>
2026-09-23 20:15:48 +0700

    Задание 1

---
+++ src/Button.java
@@ -0,0 +1,9 @@
+public class Button {
+
+    int count = 0;
+
+    void click() {
+        count++;
+        System.out.println(count);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Задание 1
+        System.out.println("<--- Задание 1: Начало --->");
+        Button button = new Button();
+
+        button.click();
+        button.click();
+        System.out.println("<--- Задание 1: Конец --->");
+
     }
 }
